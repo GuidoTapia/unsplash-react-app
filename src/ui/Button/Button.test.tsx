a5b29d3d68a723c7743bf36a3e6bd1a36235537d
GuidoTapia <31198370+GuidoTapia@users.noreply.github.com>
2024-06-06 04:34:56 -0500

    chore: add testing tools and setup (#2)

---
+++ src/ui/Button/Button.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import { Button } from './Button'
+
+const onClick = jest.fn()
+
+const buttonLabel= "Test"
+
+test('Button component example test', async () => {
+  render(<Button label={buttonLabel} data-testid="test-button" onClick={onClick}/>)
+
+  const button = screen.getByTestId('test-button')
+
+  expect(button).toHaveTextContent(buttonLabel)
+
+  await userEvent.click(button)
+})
